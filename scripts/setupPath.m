6622432fc753cf219f24222d2b304690ed4334bb
jadecastro <jdc1177@gmail.com>
2016-01-23 08:05:18 -0500

    modify path script

--- scripts/setupPath.m
+++ scripts/setupPath.m
@@ -1,24 +1,14 @@
-
-% disable JxBrowser to prevent idle cpu use
-% com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
+%
+% Set up the path for the HSCC 2016 Repeatability Package.
 % 
-% addpath(genpath([pwd,'/../../HSCC2016']));
-% addpath(genpath([pwd,'/../../HSCC2016/lib/drake-distro/spotless']));
-% addpath([pwd,'/../../HSCC2016/lib/drake-distro/drake/examples/DubinsCar']);
-% addpath([pwd,'/../../HSCC2016/lib/drake-distro/drake']);
-% 
-% addpath(genpath('/home/jon/Software/mosek'));
-% 
-% run('/home/jon/Software/drake-distro/drake/addpath_drake');
-
 
 addpath(genpath([pwd,'/../../HSCC2016']));
-rmpath(genpath([pwd,'/../../HSCC2016/lib/drake-distro-lcm-win64']));
-run('/home/jon/Software/drake-distro/drake/addpath_drake');
+addpath(genpath([pwd,'/../../HSCC2016/lib/drake-distro/spotless']));
+addpath([pwd,'/../../HSCC2016/lib/drake-distro/drake']);
+
 addpath(genpath('/home/jon/Software/mosek'));
-addpath(genpath('/home/jon/Software/drake-distro/spotless'));
-addpath('/home/jon/Software/drake-distro/drake/examples/DubinsCar');
-addpath('/home/jon/Software/drake-distro/drake');
-rmpath([pwd,'/../../HSCC2016/lib/drake-distro-lcm-win64/spotless']);
-rmpath([pwd,'/../../HSCC2016/lib/spotless-master']);
+
+run('/home/jon/Software/drake-distro/drake/addpath_drake');
+
+
 
