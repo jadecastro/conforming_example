456d643651d0f470d014a0b0a179dd165f9eb1e2
jadecastro <jdc1177@gmail.com>
2016-02-09 08:01:23 -0500

    update path script

--- scripts/setupPath.m
+++ scripts/setupPath.m
@@ -12,7 +12,14 @@ addpath(genpath([pwd,'/../lib/mosek']));
 
 run([pwd,'/../lib/drake-distro/drake/addpath_drake']);
 
+% Comment/uncomment either of the following if you get 'not found on path' warnings when attempting to remove the examples folder 
+rmpath(genpath([pwd,'/../lib/drake-distro/drake/examples']));
+%rmpath(genpath(GetFullPath([pwd,'/../lib/drake-distro/drake/examples'])));
+
+addpath([pwd,'/../lib/drake-distro/drake/examples/DubinsCar']);
+
 % If installing ellipsoids version 1.1.3 and MPT version 2.6.3, these packages come bundled with SeDuMi, 
 % which may cause errors (e.g. invalid MEX file errors). To remedy this, remove them from the path:
 rmpath([pwd,'/../lib/ellipsoids/solvers/SeDuMi_1_1']);
 rmpath([pwd,'/../lib/mpt/solvers/SeDuMi_1_3']);
+
